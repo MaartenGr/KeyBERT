9ee96f98b997d5e79f010219f30e870a01d4442c
MaartenGr <maarten_grootendorst@hotmail.com>
2021-06-21 11:10:56 +0200

    Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,21 +1,24 @@
 import pytest
 from .utils import get_test_data
 from sklearn.feature_extraction.text import CountVectorizer
+from keybert import KeyBERT
 
 doc_one, doc_two = get_test_data()
+model = KeyBERT(model='paraphrase-MiniLM-L6-v2')
 
 
 @pytest.mark.parametrize("keyphrase_length", [(1, i+1) for i in range(5)])
 @pytest.mark.parametrize("vectorizer", [None, CountVectorizer(ngram_range=(1, 1), stop_words="english")])
-def test_single_doc(keyphrase_length, vectorizer, base_keybert):
+def test_single_doc(keyphrase_length, vectorizer):
     """ Test whether the keywords are correctly extracted """
     top_n = 5
 
-    keywords = base_keybert.extract_keywords(doc_one,
-                                             keyphrase_ngram_range=keyphrase_length,
-                                             min_df=1,
-                                             top_n=top_n,
-                                             vectorizer=vectorizer)
+    keywords = model.extract_keywords(doc_one,
+                                      keyphrase_ngram_range=keyphrase_length,
+                                      min_df=1,
+                                      top_n=top_n,
+                                      vectorizer=vectorizer)
+
     assert isinstance(keywords, list)
     assert isinstance(keywords[0], tuple)
     assert isinstance(keywords[0][0], str)
@@ -29,16 +32,16 @@ def test_single_doc(keyphrase_length, vectorizer, base_keybert):
                                                            for i in range(4)
                                                            for truth in [True, False]])
 @pytest.mark.parametrize("vectorizer", [None, CountVectorizer(ngram_range=(1, 1), stop_words="english")])
-def test_extract_keywords_single_doc(keyphrase_length, mmr, maxsum, vectorizer, base_keybert):
+def test_extract_keywords_single_doc(keyphrase_length, mmr, maxsum, vectorizer):
     """ Test extraction of protected single document method """
     top_n = 5
-    keywords = base_keybert._extract_keywords_single_doc(doc_one,
-                                                         top_n=top_n,
-                                                         keyphrase_ngram_range=keyphrase_length,
-                                                         use_mmr=mmr,
-                                                         use_maxsum=maxsum,
-                                                         diversity=0.5,
-                                                         vectorizer=vectorizer)
+    keywords = model._extract_keywords_single_doc(doc_one,
+                                                  top_n=top_n,
+                                                  keyphrase_ngram_range=keyphrase_length,
+                                                  use_mmr=mmr,
+                                                  use_maxsum=maxsum,
+                                                  diversity=0.5,
+                                                  vectorizer=vectorizer)
     assert isinstance(keywords, list)
     assert isinstance(keywords[0][0], str)
     assert isinstance(keywords[0][1], float)
@@ -48,12 +51,12 @@ def test_extract_keywords_single_doc(keyphrase_length, mmr, maxsum, vectorizer, 
 
 
 @pytest.mark.parametrize("keyphrase_length", [(1, i+1) for i in range(5)])
-def test_extract_keywords_multiple_docs(keyphrase_length, base_keybert):
+def test_extract_keywords_multiple_docs(keyphrase_length):
     """ Test extractino of protected multiple document method"""
     top_n = 5
-    keywords_list = base_keybert._extract_keywords_multiple_docs([doc_one, doc_two],
-                                                                 top_n=top_n,
-                                                                 keyphrase_ngram_range=keyphrase_length)
+    keywords_list = model._extract_keywords_multiple_docs([doc_one, doc_two],
+                                                          top_n=top_n,
+                                                          keyphrase_ngram_range=keyphrase_length)
     assert isinstance(keywords_list, list)
     assert isinstance(keywords_list[0], list)
     assert len(keywords_list) == 2
@@ -65,16 +68,16 @@ def test_extract_keywords_multiple_docs(keyphrase_length, base_keybert):
             assert len(keyword[0].split(" ")) <= keyphrase_length[1]
 
 
-def test_error(base_keybert):
+def test_error():
     """ Empty doc should raise a ValueError """
     with pytest.raises(AttributeError):
         doc = []
-        base_keybert._extract_keywords_single_doc(doc)
+        model._extract_keywords_single_doc(doc)
 
 
-def test_empty_doc(base_keybert):
+def test_empty_doc():
     """ Test empty document """
     doc = ""
-    result = base_keybert._extract_keywords_single_doc(doc)
+    result = model._extract_keywords_single_doc(doc)
 
     assert result == []
