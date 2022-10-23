5b04ad28c391a9e02ee902fba90f3bb7169eaa08
rafi.martirosyan@picsart.com <rafi.martirosyan@picsart.com>
2022-10-23 19:11:08 +0400

    torch directory

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
-from keybert._model import KeyBERT
+import os
+import pathlib
+from ._model import KeyBERT
 
 __version__ = "0.6.0"
+os.environ['TORCH_HOME'] = pathlib.Path(__file__).resolve().parent.as_posix()
