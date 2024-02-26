f38d5c95828d41b6525235899bf17cbf228002bb
Anurag Dhadse <hello@adhadse.com>
2024-02-26 07:11:51 +0530

    Opens sentence transformer backend to edit batch_size param
    - updates flake8 pre-commit-config to use github repo

--- keybert/__init__.py
+++ keybert/__init__.py
@@ -1,4 +1,5 @@
 from keybert._llm import KeyLLM
 from keybert._model import KeyBERT
+from keybert.backend import SentenceTransformerBackend
 
 __version__ = "0.8.4"
---
+++ tests/test_backend.py
@@ -0,0 +1,41 @@
+import pytest
+from keybert import KeyBERT
+from keybert.backend import SentenceTransformerBackend
+import sentence_transformers
+
+from sklearn.feature_extraction.text import CountVectorizer
+from .utils import get_test_data
+
+
+doc_one, doc_two = get_test_data()
+
+
+@pytest.mark.parametrize("keyphrase_length", [(1, i + 1) for i in range(5)])
+@pytest.mark.parametrize(
+    "vectorizer", [None, CountVectorizer(ngram_range=(1, 1), stop_words="english")]
+)
+def test_single_doc_sentence_transformer_backend(keyphrase_length, vectorizer):
+    """Test whether the keywords are correctly extracted"""
+    top_n = 5
+
+    st_model = sentence_transformers.SentenceTransformer(
+        "all-MiniLM-L6-v2", device="cpu"
+    )
+
+    model = KeyBERT(backend=SentenceTransformerBackend(st_model, batch_size=128))
+
+    keywords = model.extract_keywords(
+        doc_one,
+        keyphrase_ngram_range=keyphrase_length,
+        min_df=1,
+        top_n=top_n,
+        vectorizer=vectorizer,
+    )
+
+    assert isinstance(keywords, list)
+    assert isinstance(keywords[0], tuple)
+    assert isinstance(keywords[0][0], str)
+    assert isinstance(keywords[0][1], float)
+    assert len(keywords) == top_n
+    for keyword in keywords:
+        assert len(keyword[0].split(" ")) <= keyphrase_length[1]
